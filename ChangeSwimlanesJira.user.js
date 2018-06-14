1b53ffc52692fe3b3ec8bee7524ead6952c99b12
Wladyslaw Wolinski <wladyslaw.wolinski@lhsystems.com>
2018-06-14 08:40:45 +0200

    bump version

--- ChangeSwimlanesJira.user.js
+++ ChangeSwimlanesJira.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChangeSwimlanesJira
 // @namespace    http://tampermonkey.net/
-// @version      0.7
+// @version      0.8
 // @description  try to take over the world!
 // @author       WLAD
 // @updateSite https://github.com/tepesware/TepesColors/raw/master/ChangeSwimlanesJira.user.js
