1455579990922b35eb9bd33ee4d20389ecd04bc2
Wladyslaw Wolinski <wladyslaw.wolinski@lhsystems.com>
2018-07-03 10:54:40 +0200

    added TODO

--- ChangeSwimlanesJira.user.js
+++ ChangeSwimlanesJira.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         ChangeSwimlanesJira
 // @namespace    http://tampermonkey.net/
-// @version      0.9
+// @version      1.0
 // @description  try to take over the world!
 // @author       WLAD
 // @updateSite https://github.com/tepesware/TepesColors/raw/master/ChangeSwimlanesJira.user.js
@@ -12,6 +12,8 @@
 
 (function() {
     'use strict';
+
+
     function addGlobalStyle(css) {
         var head, style;
         head = document.getElementsByTagName('head')[0];
@@ -21,11 +23,21 @@
         style.innerHTML = css;
         head.appendChild(style);
     }
+
     addGlobalStyle('.ghx-swimlane-header.ghx-done{ background-color: #8bc34a;border-style: groove; border-width:1px;}');
     addGlobalStyle('.ghx-swimlane-header.ghx-done.ghx-selected{ background-color: #8bc34a;border-style: groove; border-width:2px;}');
-    addGlobalStyle('.ghx-swimlane-header { background-color: #F2f292;border-style: groove; border-width:1px;}');
+    addGlobalStyle('.ghx-swimlane-header { background-color: #ffd351cc;border-style: groove; border-width:1px;}');
     addGlobalStyle('.ghx-swimlane-header.ghx-selected{ background-color: #F2f292 ;border-style: solid; border-width:2px ;}');
 
+
+    $("body").on('DOMSubtreeModified', "#content", function() {
+        var swimlanes = document.getElementsByClassName("ghx-info");
+        for(var i=0;i<swimlanes.length;i++){
+            if(swimlanes[i].getElementsByTagName("span")[0].textContent == "To Do"){
+                $(document.getElementsByClassName("ghx-info")[i].parentElement.parentElement).css('background-color', "rgba(33, 150, 243, 0.55)");
+            }
+        }
+    });
 
 
 })();
