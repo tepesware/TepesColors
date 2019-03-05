b80d6d532a6ac5b72babd8017710297f2ae6859e
Wladyslaw Wolinski <wladyslaw.wolinski@lhsystems.com>
2019-03-05 09:37:52 +0100

    updated code

--- RemoveAutomatedRows.js
+++ RemoveAutomatedRows.js
@@ -1,24 +1,54 @@
 // ==UserScript==
-// @name         Show Test Status
+// @name         Remove Automated rows
 // @namespace    http://tampermonkey.net/
-// @version      1.0
+// @version      0.1
 // @description  try to take over the world!
 // @author       WLAD
-// @updateSite https://github.com/tepesware/TepesColors/raw/master/tests.js
-// @downloadURL https://github.com/tepesware/TepesColors/raw/master/tests.js
-
+// @updateSite https://github.com/tepesware/TepesColors/raw/master/RemoveAutomatedRows.js
+// @downloadURL https://github.com/tepesware/TepesColors/raw/master/RemoveAutomatedRows.js
+// @match        none
 // @grant        none
 // ==/UserScript==
 
-var done = false;
-(function () {
+(function() {
     'use strict';
 
 
 
-    $('#exec-tests-progressbar').insertAfter('#page');
-    $($('.aui-page-header')[1]).insertAfter('#page');
-    $('#page').remove();
+
+
+    var observer = new MutationObserver(function (mutations) {
+        // For the sake of...observation...let's output the mutation to console to see how this all works
+        mutations.forEach(function (mutation) {
+            // debugger
+
+
+
+
+
+            if (mutation.addedNodes.length > 0) {
+                console.log(mutation.target);
+                $('.lozenge[title="Automated"]').closest('tr').remove();
+
+            }
+
+        });
+    });
+
+
+    // Notify me of everything!
+    var observerConfig = {
+        attributes: false,
+        childList: true,
+        characterData: false,
+        subtree:true
+    };
+
+// Node, config
+// In this case we'll listen to all changes to body and child nodes
+    var targetNode = document.getElementById("testexec-tests-table-wrapper");
+    // debugger
+    observer.observe(targetNode, observerConfig);
+
 
 })();
-
