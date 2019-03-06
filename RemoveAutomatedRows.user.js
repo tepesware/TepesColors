12d880ae7a8c8310baaa1c0e768a0d722230d73e
Wladyslaw Wolinski <wladyslaw.wolinski@lhsystems.com>
2019-03-06 11:52:54 +0100

    show manual count

--- RemoveAutomatedRows.user.js
+++ RemoveAutomatedRows.user.js
@@ -1,54 +1,57 @@
 // ==UserScript==
-// @name         Remove Automated rows
+// @name         Show Test Status
 // @namespace    http://tampermonkey.net/
-// @version      0.3
+// @version      1.0
 // @description  try to take over the world!
 // @author       WLAD
-// @updateSite https://raw.githubusercontent.com/tepesware/TepesColors/master/RemoveAutomatedRows.user.js
-// @downloadURL https://raw.githubusercontent.com/tepesware/TepesColors/master/RemoveAutomatedRows.user.js
-// @match        none
+// @updateSite https://github.com/tepesware/TepesColors/raw/master/tests.js
+// @downloadURL https://github.com/tepesware/TepesColors/raw/master/tests.js
+
 // @grant        none
 // ==/UserScript==
 
-(function() {
+var done = false;
+(function () {
     'use strict';
 
 
 
+    $('#exec-tests-progressbar').insertAfter('#page');
+    $($('.aui-page-header')[1]).insertAfter('#page');
+    $('#page').remove();
 
 
-    var observer = new MutationObserver(function (mutations) {
-        // For the sake of...observation...let's output the mutation to console to see how this all works
-        mutations.forEach(function (mutation) {
-            // debugger
+    $.ajax({
 
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        url: "https://trackspace.lhsystems.com/rest/raven/1.0/testexec/ERMAMM-2447/test?detailed=true",
+        data: "{}",
+        dataType: "json",
+        success: function (data) {
 
-
-
-
-            if (mutation.addedNodes.length > 0) {
-                console.log(mutation.target);
-                $('.lozenge[title="Automated"]').closest('tr').remove();
+            var entries = data.entries;
+            var toDoNotAutomated = 0;
+            for (var i = 0; i < entries.length; i++) {
+                if(entries[i].status.name == "TODO"){
+                    var lables = entries[i].userColumns.labels;
+                    var isAutomated = lables.includes("<span>Automated</span>");
+                    if (!isAutomated){
+                        toDoNotAutomated++;
+                    }
+                };
 
             }
-
-        });
+            $('#exec-tests-progressbar > div').append('<span style="color:#A2A6AE" class="testexec-status-count">'+toDoNotAutomated+'</span>');
+            $('#exec-tests-progressbar > div').append('<span class="testexec-status-name">MANUAL</span>');
+            console.log("Liczba automated " + toDoNotAutomated);
+        },
+        error: function (result) {
+            console.log("Error " + result);
+            //alert("Error");
+        }
     });
 
 
-    // Notify me of everything!
-    var observerConfig = {
-        attributes: false,
-        childList: true,
-        characterData: false,
-        subtree:true
-    };
-
-// Node, config
-// In this case we'll listen to all changes to body and child nodes
-    var targetNode = document.getElementById("testexec-tests-table-wrapper");
-    // debugger
-    observer.observe(targetNode, observerConfig);
-
-
 })();
+
