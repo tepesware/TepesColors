98899d1acfe3a4c2bda96094ff39b68da7569df0
Wladyslaw Wolinski <wladyslaw.wolinski@lhsystems.com>
2019-03-21 08:36:15 +0100

    added include as the whole page and now the sript is just watching for given tag

--- RemoveAutomatedRows.user.js
+++ RemoveAutomatedRows.user.js
@@ -1,51 +1,53 @@
 // ==UserScript==
 // @name         Remove Automated rows
 // @namespace    http://tampermonkey.net/
-// @version      0.6
+// @version      0.7
 // @description  try to take over the world!
 // @author       WLAD
-// @updateSite https://raw.githubusercontent.com/tepesware/TepesColors/master/RemoveAutomatedRows.user.js
-// @downloadURL https://raw.githubusercontent.com/tepesware/TepesColors/master/RemoveAutomatedRows.user.js
-// @match        none
+// @updateSite   https://raw.githubusercontent.com/tepesware/TepesColors/master/RemoveAutomatedRows.user.js
+// @downloadURL  https://raw.githubusercontent.com/tepesware/TepesColors/master/RemoveAutomatedRows.user.js
+// @include        /https:\/\/trackspace.lhsystems.com\/browse\/ERMAMM.*/
 // @grant        none
 // ==/UserScript==
 
 (function() {
     'use strict';
 
- var issueId= $('head > meta[name=ajs-issue-key]')[0].content;
+   function updateManualTestNumber(){
+        var issueId= $('head > meta[name=ajs-issue-key]')[0].content;
 
-     $.ajax({
+    $.ajax({
 
-                type: "GET",
-                contentType: "application/json; charset=utf-8",
-                url: "https://trackspace.lhsystems.com/rest/raven/1.0/testexec/"+issueId+"/test?detailed=true",
-                data: "{}",
-                dataType: "json",
-                success: function (data) {
+        type: "GET",
+        contentType: "application/json; charset=utf-8",
+        url: "https://trackspace.lhsystems.com/rest/raven/1.0/testexec/"+issueId+"/test?detailed=true",
+        data: "{}",
+        dataType: "json",
+        success: function (data) {
 
-                    var entries = data.entries;
-                    var toDoNotAutomated = 0;
-                    for (var i = 0; i < entries.length; i++) {
-                        if(entries[i].status.name == "TODO"){
-                            var lables = entries[i].userColumns.labels;
-                            var isAutomated = lables.includes("<span>Automated</span>");
-                            var isscriptNotUpdated =lables.includes("<span>scriptNotUpdated</span>");
-                            if (!isAutomated || isscriptNotUpdated){
-                                toDoNotAutomated++;
-                            }
-                        };
-
+            var entries = data.entries;
+            var toDoNotAutomated = 0;
+            for (var i = 0; i < entries.length; i++) {
+                if(entries[i].status.name == "TODO"){
+                    var lables = entries[i].userColumns.labels;
+                    var isAutomated = lables.includes("<span>Automated</span>");
+                    var isscriptNotUpdated =lables.includes("<span>scriptNotUpdated</span>");
+                    if (!isAutomated || isscriptNotUpdated){
+                        toDoNotAutomated++;
                     }
-                    $('#exec-tests-progressbar > div').append('<span style="color:#A2A6AE" class="testexec-status-count">'+toDoNotAutomated+'</span>');
-                    $('#exec-tests-progressbar > div').append('<span class="testexec-status-name">MANUAL</span>');
-                    console.log("Liczba automated " + toDoNotAutomated);
-                },
-                error: function (result) {
-                    console.log("Error " + result);
-                    //alert("Error");
-                }
-            });
+                };
+
+            }
+            $('#exec-tests-progressbar > div').append('<span style="color:#A2A6AE" class="testexec-status-count">'+toDoNotAutomated+'</span>');
+            $('#exec-tests-progressbar > div').append('<span class="testexec-status-name">MANUAL</span>');
+            console.log("Liczba automated " + toDoNotAutomated);
+        },
+        error: function (result) {
+            console.log("Error " + result);
+            //alert("Error");
+        }
+    });
+   }
 
 
 
@@ -58,8 +60,7 @@
             if (mutation.addedNodes.length > 0) {
                 console.log(mutation.target);
 
-                $('.lozenge[title="Automated"]').closest('tr').not($('.lozenge[title="scriptNotUpdated"]').closest('tr')).remove();
-			
+                $('.lozenge[title="Automated"]').closest('tr').not($('.lozenge[title="scriptNotUpdated"]').closest('tr')).remove();		
             }
         });
     });
@@ -73,11 +74,14 @@
         subtree:true
     };
 
-// Node, config
-// In this case we'll listen to all changes to body and child nodes
+    // Node, config
+    // In this case we'll listen to all changes to body and child nodes
     var targetNode = document.getElementById("testexec-tests-table-wrapper");
-    // debugger
-    observer.observe(targetNode, observerConfig);
+    debugger
+    if(targetNode != null){
+        observer.observe(targetNode, observerConfig);
+        updateManualTestNumber();
+    }
 
 
 })();
